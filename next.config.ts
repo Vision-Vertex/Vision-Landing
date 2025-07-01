1ac0b3eb868a306befc195808582d74b803d0c08
abiywondimu5758 <abiywondimu1@gmail.com>
2025-07-01 12:17:26 +0300

    config update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    runtime: 'nodejs', // Force Node.js runtime instead of Edge
+  },
   images: {
-    // domains: ['images.unsplash.com'],
-    unoptimized: true,
+    unoptimized: true, // Avoid sharp
   },
 };
 
