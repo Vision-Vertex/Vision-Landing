61f8442c5ab2aeb641da7303b92967cd25946c4c
abiywondimu5758 <abiywondimu1@gmail.com>
2025-07-01 12:44:10 +0300

    config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: false,
   images: {
     domains: ['images.unsplash.com'],
     
