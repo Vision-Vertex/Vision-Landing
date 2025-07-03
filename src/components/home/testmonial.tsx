d825bd4a6ad64e98af83eaac097f26bb80e15f2e
BilenAssres <bilenassres@gmail.com>
2025-07-03 20:42:50 +0300

    Refine testimonial component layout for responsive design adjustments

--- src/components/home/testmonial.tsx
+++ src/components/home/testmonial.tsx
@@ -6,7 +6,7 @@ import { testmonial as testimonialList } from '@/constants/data';
 
 function testmonial() {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 gap-24 px-6 md:px-28 py-10">
+    <div className="h-screen grid grid-cols-1 lg:grid-cols-2 gap-24 px-6 ld:px-28 py-10">
       <div className="space-y-2 mt-10">
         <div className="font-medium text-secondary uppercase">Testmonials</div>
         <div className="text-4xl text-primary font-medium">
@@ -27,12 +27,12 @@ function testmonial() {
         <Image
           src={TestmonialIcon}
           alt="Icon"
-          className="w-full h-[45rem] absolute -z-10 md:block hidden"
+          className="w-full h-[45rem] absolute -z-10 lg:block hidden"
           width={200}
           height={150}
         />
         <div className='flex items-start gap-6 h-[45rem] pt-10'>
-          <div className="ml-28 flex flex-col items-center justify-center md:block hidden">
+          <div className="ml-28 flex flex-col items-center justify-center lg:block hidden">
             <Image
               src={testimonialList[0].image}
               alt="Icon"
@@ -43,7 +43,7 @@ function testmonial() {
             <div className="h-56 w-2 bg-secondary "></div>
           </div>
           <div className="z-40  px-5 pt-10">
-            <div className="text-xs text-black  md:text-white">
+            <div className="text-xs text-black  lg:text-white">
               " {testimonialList[0].quote} "
             </div>
             <div className='text-sm text-secondary text-right mt-3 font-bold'> - {testimonialList[0].name}</div>
